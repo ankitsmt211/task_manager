82b1e13d084683eed8b69b04767454c962e944a3
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-10-22 11:28:49 +0530

    API to create task

--- tasks/views/create_task.py
+++ tasks/views/create_task.py
@@ -1,8 +1,21 @@
+from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from tasks.models import Task
+
+
+class SerializerTask(serializers.ModelSerializer):
+
+    class Meta:
+        model = Task
+        exclude = ['created_at']
+
 
 @api_view(['POST'])
-def create_task(request):
-    # create a task
-    return Response('Task created')
+def create_task(request, *args, **kwargs):
+    serializer = SerializerTask(data=request.data)
+    if not serializer.is_valid():
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer.save()
+    return Response(data=serializer.data, status=status.HTTP_201_CREATED)
