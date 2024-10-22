889b644d5e45f9f57020c192b3f87cd5b4a57988
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-10-22 11:29:42 +0530

    API view for task list by user

--- tasks/views/task_list.py
+++ tasks/views/task_list.py
@@ -1,6 +1,26 @@
+from rest_framework import status, serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from tasks.models import Task
+from users.models import User
+
+
+class SerializerTaskList(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
 @api_view(['GET'])
-def get_task_list_by_user(request):
+def get_task_list_by_user(request, *args, **kwargs):
     # fetch task list for a specific user and return
-    return Response('Task list')
+    user_id = kwargs.get('user_id')
+    if not user_id:
+        return Response(data='Bad URL', status=status.HTTP_400_BAD_REQUEST)
+
+    user_obj = User.objects.filter(pk=user_id).first()
+    if not user_obj:
+        return Response(data='Invalid User ID', status=status.HTTP_400_BAD_REQUEST)
+    serializer = SerializerTaskList(instance=user_obj.tasks, many=True)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
