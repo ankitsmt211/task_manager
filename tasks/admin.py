d7e2418c1144a2b2dd470f81b45eb73f30d227bf
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-10-22 11:25:58 +0530

    setup admin interface for both models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from tasks.models import Task
+
+
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'status')
+    exclude = ['created_at']
+    raw_id_fields = ('users',)
+
+
+admin.site.register(Task, TaskAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from users.models import User
+
+
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'role')
+    exclude = ['created_at', 'updated_at']
+
+
+admin.site.register(User, UserAdmin)
